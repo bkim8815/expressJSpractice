15b035c63163a6cd69f4f98dbed867e0abba3af5
Brian Kim <bkim8815@gmail.com>
2016-04-11 17:21:29 -0400

    updated package json

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Brian Kim', name: 'kellough' });
 });
 
 module.exports = router;
